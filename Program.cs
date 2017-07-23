ec2991fbe6581342d3672fe690c7be59716cf8a1
andres naranjo <andresnaranjo@afnarqui.com>
2017-07-23 09:08:34 -0500

    versión inicial Program.cs

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using Opa.Framework.Integration.Jobs;
+using Opa.Framework.Integration.Jobs.Tasks;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //CargarSeguimientos c = new CargarSeguimientos();
+            
+            
+            
+            //c.CargarDatosGeneralPersonasAsync();
+            //c.CargarCreditosPersonaAsync();
+            //c.CargarPerdidaEsperadaAsync();
+
+
+            //CargarSeguimientos c = new CargarSeguimientos();
+            //c.CargarPerdidaEsperadaAsync();
+
+            //CargarObligaciones c = new CargarObligaciones();
+            //c.CargarTemporalObligacionesAsync();
+
+            //CargarSeguimientos c = new CargarSeguimientos();
+            //c.CargarScoreSeguimientoAsync(false);
+
+            //CargarSeguimientos c = new CargarSeguimientos();
+            //c.CargarAnalisisDePortafolioAsync();
+
+            ////CargarSeguimientos c = new CargarSeguimientos();
+            ////c.CargarCreditosPersonaAsync();
+
+            //////////CargarSeguimientos c = new CargarSeguimientos();
+            //////////c.CargarCreditosPersonaAsync();
+
+
+            CargarSeguimientos c = new CargarSeguimientos();
+
+            CargarObligaciones co = new CargarObligaciones();
+
+            CargarOtorgamientoMasivo ot = new CargarOtorgamientoMasivo();
+
+            CargarCalificacionCartera cal = new CargarCalificacionCartera();
+           
+            //c.SeguimientoprovisionPersonaAsync();
+
+            //c.CargarPerdidaEsperadaAsync();
+            //c.CargarCreditosPersonaAsync();
+
+            //c.CargarScoreSeguimientoAsync();
+            //c.CalificacionCartera();
+
+            //cal.CargarScoreSeguimientoAsync();
+            cal.CalificacionCartera();
+            //co.CargarTemporalObligacionesAsync();
+
+
+            //ot.CargarScoreSeguimientoAsync(true);
+
+            //c.CargarDatosGeneralPersonasAsync();
+            //co.CargarTemporalDatosPersonasAsync();
+        }
+    }
+}
